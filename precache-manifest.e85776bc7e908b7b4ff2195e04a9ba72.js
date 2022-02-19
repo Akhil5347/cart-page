c43b4e25b801bb1eaf53c783ed73716184331a4f
Akhil <mgakhil03@gmail.com>
2022-02-19 11:44:20 +0530

    Updates

---
+++ precache-manifest.e85776bc7e908b7b4ff2195e04a9ba72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5e2ff15d2d5b1ea6ded60a3f69861",
+    "url": "/cart-page/index.html"
+  },
+  {
+    "revision": "011cae29ffce5857d671",
+    "url": "/cart-page/static/css/main.29894cfc.chunk.css"
+  },
+  {
+    "revision": "7f031df770cb41e85cc8",
+    "url": "/cart-page/static/js/2.7996493d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/cart-page/static/js/2.7996493d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e30fbcb1ac4cc40f5a",
+    "url": "/cart-page/static/js/3.728523d4.chunk.js"
+  },
+  {
+    "revision": "011cae29ffce5857d671",
+    "url": "/cart-page/static/js/main.c6287871.chunk.js"
+  },
+  {
+    "revision": "ddbcc67c3da0f5c4be81",
+    "url": "/cart-page/static/js/runtime-main.c0e260d7.js"
+  }
+]);
